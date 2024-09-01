2d31646fcdf84ddf2b95cf71c1acd769b79680d2
Fanaticmouse221 <jacktennis21@gmail.com>
2024-09-01 13:41:45 +1000

    Update channelInfo.js

--- channelInfo.js
+++ channelInfo.js
@@ -87,7 +87,7 @@ const channelInfo = [{
     id: 13,
     name: 'Super Sports Football',
     img: 'https://res.cloudinary.com/djecqwc0z/image/upload/v1694660323/sky-sports-golf_nnwxlo.jpg',
-    url: '/skysports-golf',
+    url: '/supersport-football',
     iframe: '<iframe src="//stream.crichd.vip/update/ssfotbal.php" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>'
 },
 {
